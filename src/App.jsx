3b0bc3cec21f678a17a4853a44747fa0c2bf099a
Saul Gabriel <saulgabreil069@gmail.com>
2024-03-23 19:36:38 -0300

    segundo commit do App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import iconSedans from "./assets/icon-sedans.svg"
+import iconSuvs from "./assets/icon-suvs.svg"
+import iconLuxury from "./assets/icon-luxury.svg"
+
 export default  function App(){
   return(
-    <h1>Hello World</h1>
+    <main>
+      <section>
+        <div>
+          <img src={iconSedans} alt="icone dos carros sedans"/>
+          <h2>Sedans</h2>
+          <p>Escolha um sedan pelo seu preço acessivel e excelente economia de combustivel. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+          <a href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
+        </div>
+
+        <div>
+          <img src={iconSuvs} alt="icone dos carros SUV"/>
+          <h2>SUVs</h2>
+          <p>Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+          <a href="https://www.webmotors.com.br/suvs" target="_blank">Ver mais</a>
+        </div>
+
+        <div>
+          <img src={iconLuxury} alt="icone dos carros de Luxo"/>
+          <h2>Luxuosos</h2>
+          <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+          <a href="https://www.webmotors.com.br/carros-de-luxo" target="_blank">Ver mais</a>
+        </div>
+      </section>
+    </main>
   )
 }
