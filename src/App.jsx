433b598885f03f9deb6079c669f60f5cf7360983
Saul Gabriel <saulgabreil069@gmail.com>
2024-03-23 22:49:51 -0300

    Terceiro Commit do App

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,21 @@ export default  function App(){
   return(
     <main>
       <section>
-        <div>
+        <div className="bg-orange">
           <img src={iconSedans} alt="icone dos carros sedans"/>
           <h2>Sedans</h2>
           <p>Escolha um sedan pelo seu preço acessivel e excelente economia de combustivel. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
           <a href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
         </div>
 
-        <div>
+        <div className="bg-green">
           <img src={iconSuvs} alt="icone dos carros SUV"/>
           <h2>SUVs</h2>
           <p>Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
           <a href="https://www.webmotors.com.br/suvs" target="_blank">Ver mais</a>
         </div>
 
-        <div>
+        <div className="bg-dark-green">
           <img src={iconLuxury} alt="icone dos carros de Luxo"/>
           <h2>Luxuosos</h2>
           <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,11 @@ export default {
       'offwhite' : 'rgba(255,255,255,0.75)',
       'white' : '#fff',
       'offwhitebg' : '#f2f2f2'
-    } 
+    } ,
+    fontFamily:{
+      'sholders' : '"Big Shoulders Display"',
+      'lexend' : '"Lexend Deca"',
+    }
   },
   plugins: [],
 }
